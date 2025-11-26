fed5f5ebd21a3e9fd69ea62552fcb643b6f02849
bjhardcastle <ben.hardcastle@alleninstitute.org>
2025-11-26 15:47:11 -0800

    Update recent

--- dr/photodiode.py
+++ dr/photodiode.py
@@ -120,180 +120,7 @@ def get_frame_on_off_samples(diode, rate):
 
     return on, off
 
-
-def discretize_vsync_times(
-    vsync_times: npt.NDArray,
-    on_flip_times: npt.NDArray,
-    is_first_frame_on: bool,
-    frame_rate: float = 60.0,
-) -> npt.NDArray:
-    """
-    Discretize vsync times to multiples of 1/frame_rate, using photodiode signals
-    to determine long frames, whether they are visible in vsync intervals or not.
-        
-    >>> on_frame_times = np.array([1, 4, 6, 10]) * 1/60
-
-    When vsyncs do not show long intervals:
-    >>> discretize_vsync_times(np.array([0, 1, 2, 3, 4, 5, 6]) * 1/60, on_frame_times, is_first_frame_on=True) * 60
-    array([0. , 1.5, 3. , 4. , 5. , 7. , 9. ])
-    
-    When vsyncs show long intervals:
-    >>> discretize_vsync_times(np.array([0, 1, 3, 4, 5, 6, 9]) * 1/60, on_frame_times, is_first_frame_on=True) * 60
-    array([0., 1., 3., 4., 5., 6., 9.])
-    
-    When first frame is an OFF transition we can't adjust the first vsync time:
-    >>> discretize_vsync_times(np.array([0, 1, 2, 3, 4, 5, 6, 7]) * 1/60, on_frame_times, is_first_frame_on=False) * 60
-    array([ 0. ,  1. ,  2.5,  4. ,  5. ,  6. ,  8. , 10. ])
-    """
-    frame_duration = 1.0 / frame_rate
-    
-    # Get first vsync time as reference
-    t0 = vsync_times[0]
-    
-    if not is_first_frame_on:
-        # If the first frame is off, we can't adjust the first vsync time - discard it for now
-        vsync_times = vsync_times[1:]
-
-    flip_intervals = np.diff(on_flip_times)
-    # Note: flip_intervals represents 2 frames duration (one on_frame + one off_frame)
-    expected_interval_frames = 2  # Expected number of frames between consecutive on_frame signals
-    
-    # Identify long photodiode intervals
-    long_intervals = []
-    for i, interval_dur in enumerate(flip_intervals):
-        # Calculate how many frames this interval spans (expected is 2 frames)
-        n_frames_in_interval = round(interval_dur / (frame_duration), ndigits=1)
-        
-        assert n_frames_in_interval >= expected_interval_frames, (
-            f"Short photodiode interval at index {i} - should not happen if photodiode signal has been properly filtered: {interval_dur:.4f}s ≈ {interval_dur/frame_duration:.2f} frames"
-        ) 
-        assert n_frames_in_interval == round(n_frames_in_interval), f"Interval duration is not a multiple of frame duration (within +-0.1x): {interval_dur:.4f}s ≈ {interval_dur/frame_duration:.2f} frames"
-        if n_frames_in_interval > expected_interval_frames:
-            long_intervals.append((i, round(n_frames_in_interval)))
-                
-    # initial discretization of vsync times assuming no dropped frames
-    initial_frame_indices = np.arange(len(vsync_times), dtype=float)
-
-    adjusted_frame_indices = initial_frame_indices.copy()
-    
-    # Process each abnormal interval to determine actual frame timing
-    for i, diode_n_frames in long_intervals:
-
-        # get indices of vsyncs for on and off frames
-        vsync_idx = (expected_interval_frames * i, (expected_interval_frames * i) + 1)
-
-        vsync_intervals = np.diff(vsync_times[vsync_idx[0]: vsync_idx[0] + 3])
-        
-        vsync_n_frames = [np.round(interval / frame_duration) for interval in vsync_intervals]
-        assert len(vsync_n_frames) == 2
-        assert not np.any(vsync_n_frames == 0)
-        
-        if np.sum(vsync_n_frames) == diode_n_frames:
-            # use the n frames estimated from vsync intervals to adjust the off-frame:
-            adjustment = vsync_n_frames[0]
-            adjusted_frame_indices[vsync_idx[1]] = adjusted_frame_indices[vsync_idx[0]] + adjustment
-        else:
-            # n frames estimated from vsync intervals is not consistent with estimate from photodiode intervals
-            #! set off-frame to be at the center of the interval:
-            adjustment = (diode_n_frames / 2)
-            assert adjustment > 1, "n_frames should be > 2, otherwise it's not a long interval"
-            adjusted_frame_indices[vsync_idx[1]] = adjusted_frame_indices[vsync_idx[0]] + adjustment
-            
-        # regardless of within-interval modification, shift all subsequent frames to account for
-        # long interval:
-        adjusted_frame_indices[vsync_idx[1] + 1:] = adjusted_frame_indices[vsync_idx[1] + 1:] + (diode_n_frames - expected_interval_frames)
-            
-    if not is_first_frame_on:
-        # If the first frame is off, we need to add the first vsync time back, and we have to assume
-        # it is a single frame interval (in practice this is likely unimportant, as no events happen
-        # on the first frame of a stimulus):
-        adjusted_frame_indices = np.insert(adjusted_frame_indices + 1, 0, 0) # shift all other times by 1 frame
-        
-    # Convert adjusted frame indices back to times
-    discretized_times = t0 + adjusted_frame_indices * frame_duration
-
-    return discretized_times
-
-
-def apply_monitor_delay(discretized_vsync_times: npt.NDArray, 
-                        on_frame_times: npt.NDArray,
-                        is_first_frame_on: bool,
-                        window_sec: float = 30,
-                        fps: float = 60.0,
-                        rolling: bool = True,
-                        ) -> npt.NDArray:
-    assert 0 <= (len(discretized_vsync_times) / 2) - len(on_frame_times) <= 1
-        
-    if rolling:
-        window = np.ones(window_sec * fps)
-        if is_first_frame_on:
-            on_vsync_times = discretized_vsync_times[::2]
-        else:
-            on_vsync_times = discretized_vsync_times[1::2]
-        delay = np.convolve(on_frame_times - on_vsync_times, window, mode="same")
-        # make delay the same length as discretized_vsync_times:
-        return discretized_vsync_times + np.repeat(delay, 2)[:len(discretized_vsync_times)]
-    
-    else:
-        if is_first_frame_on:
-            delay = on_frame_times - discretized_vsync_times[::2]
-        else:
-            delay = on_frame_times - discretized_vsync_times[1::2]
-        # add the median of the delay in blocks of window_sec * fps to vsync times,
-        # making sure that the last block isn't skipped
-        adjusted_vsync_times = discretized_vsync_times.copy()
-        window_len = int(window_sec * fps * 0.5)
-        for i in range(0, len(delay), window_len):
-            if i + window_len < len(delay):
-                adjusted_vsync_times[i : i + 2*window_len] += np.median(delay[i : i + window_len])
-            else:
-                adjusted_vsync_times[i:] += np.median(delay[i:])
-        return adjusted_vsync_times
-
-
-def plot_diode_around_time(
-    event_time: float,
-    duration: float = 0.1,
-):
-    event_sample = int(
-        (event_time - sync_timing_info.start_time) * sync_timing_info.sampling_rate
-    )
-    event_sample_range = slice(
-        event_sample - int(duration * sync_timing_info.sampling_rate),
-        event_sample + int(duration * sync_timing_info.sampling_rate),
-    )
-    t = (
-        sync_timing_info.start_time
-        + np.arange(len(diode))[event_sample_range] / sync_timing_info.sampling_rate
-    )
-    plt.figure()
-    plt.plot(t, diode[event_sample_range], c="k", lw=0.5)
-    # plt.axvline(t[len(t)//2], color="k", linestyle="--", lw=.5)
-
-    def filt(array):
-        return np.array(array)[
-            (np.array(array) >= event_time - duration) & (np.array(array) <= event_time + duration)
-        ]
-    
-    def plot(array, **kwargs):
-        for t in filt(array):
-            kwargs.setdefault("linewidth", 0.5)
-            kwargs.setdefault("linestyle", "--")
-            plt.axvline(t, **kwargs)
-    
-    plot(vsync_times, color="cyan", lw=.8)
-    plot(frame_times, color="magenta")
-    # plot(np.concatenate(sync.frame_display_time_blocks) - npc_sync.MONITOR_CENTER_REFRESH_TIME, color="orange") 
-    # plot(vsync_times + 0.0182 , color="red") 
-    return plt.gcf()
-
-
-# Execute the processing
 if __name__ == "__main__":
-    import doctest
-    doctest.testmod()
-    exit()
-    
     sync = npc_sync.SyncDataset(
         "/scratch/photodiode/DRpilot_366122_20250106/20250106T191444.h5"
     )
@@ -328,14 +155,49 @@ if __name__ == "__main__":
     all_frame_times = np.sort(np.concatenate([on_frame_times, off_frame_times]))
     vsync_times = sync.get_falling_edges("vsync_stim", units="seconds")
     frame_times = []
-    for block_vsyncs in sync.vsync_times_in_blocks:
-        first = np.searchsorted(all_frame_times, block_vsyncs[0], 'right')
-        frame_times.extend(all_frame_times[first: first + len(block_vsyncs)])
+    for block in sync.vsync_times_in_blocks:
+        first = np.searchsorted(all_frame_times, block[0], 'right')
+        frame_times.extend(all_frame_times[first: first + len(block)])
     frame_times = np.array(frame_times)
 
     len(all_frame_times), len(vsync_times), len(frame_times), sum(
         [len(a) for a in sync.frame_display_time_blocks]
     )
+    def plot_diode_around_time(
+        event_time: float,
+        duration: float = 0.1,
+    ):
+        event_sample = int(
+            (event_time - sync_timing_info.start_time) * sync_timing_info.sampling_rate
+        )
+        event_sample_range = slice(
+            event_sample - int(duration * sync_timing_info.sampling_rate),
+            event_sample + int(duration * sync_timing_info.sampling_rate),
+        )
+        t = (
+            sync_timing_info.start_time
+            + np.arange(len(diode))[event_sample_range] / sync_timing_info.sampling_rate
+        )
+        plt.figure()
+        plt.plot(t, diode[event_sample_range], c="k", lw=0.5)
+        # plt.axvline(t[len(t)//2], color="k", linestyle="--", lw=.5)
+
+        def filt(array):
+            return np.array(array)[
+                (np.array(array) >= event_time - duration) & (np.array(array) <= event_time + duration)
+            ]
+        
+        def plot(array, **kwargs):
+            for t in filt(array):
+                kwargs.setdefault("linewidth", 0.5)
+                kwargs.setdefault("linestyle", "--")
+                plt.axvline(t, **kwargs)
+        
+        plot(vsync_times, color="cyan", lw=.8)
+        plot(frame_times, color="magenta")
+        # plot(np.concatenate(sync.frame_display_time_blocks) - npc_sync.MONITOR_CENTER_REFRESH_TIME, color="orange") 
+        # plot(vsync_times + 0.0182 , color="red") 
+        return plt.gcf()
 
     # plot_diode_around_time(sync.vsync_times_in_blocks[0][0], 0.15)
 
@@ -351,6 +213,5 @@ if __name__ == "__main__":
 
     # 1. discretize vsync times to multiples of 1/60
     #    - use photdiode signal to corroborate long intervals 
-    #!   - need to know if first vsync corresponds to an on or off frame 
     # 2. in epochs, estimate monitor delay (adjusted vsync time to rising edge of photodiode signal on sync)
     # 3. apply monitor delay to adjusted vsync times within epoch
